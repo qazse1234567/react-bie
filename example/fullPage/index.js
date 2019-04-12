2eac1f660db9b42aa533d1ae617b2fdf62da4e0b
ZJ <554092921@qq.com>
2019-04-12 18:01:01 +0800

    example

--- example/fullPage/index.js
+++ example/fullPage/index.js
@@ -1,24 +1,3 @@
-# react-bie
-react component
-
-***
-开发过程中写的 react 组件；逐渐更新
-***
-
-# Installing
-```
-npm install --save react-bie
-```
-or
-```
-yarn add react-bie
-```
-
-# 目前实现的组件
-
-[FullPage]
-
-```
 import React, { Component } from "react";
 import { FullPage } from "react-bie";
 import "./styles.less";
@@ -46,4 +25,3 @@ export default class FullPageExample extends Component {
         );
     }
 }
-```
